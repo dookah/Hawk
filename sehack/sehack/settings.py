c3e8f3ae36826b17889d37d30320c2cc1038b0dc
Joshua Dean <joshualukedean@gmail.com>
2021-02-01 16:33:04 +0000

    Removed state secrets

--- sehack/sehack/settings.py
+++ sehack/sehack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oco-4nj=5!0k=pd=#1&ixvy8p7tr(7dj&f#yqv9tg5na%em%&5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
