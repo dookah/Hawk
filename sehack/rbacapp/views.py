9494a414464f3ff244e6d8ba1e7c2e6596a838f0
Joshua Dean <joshualukedean@gmail.com>
2021-02-02 12:40:09 +0000

    added dotenv | added templating |

--- sehack/rbacapp/views.py
+++ sehack/rbacapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the rbacapp index.")
+
+    ctx = {
+        'name' : 'Josh'
+    }
+
+    return render(request, 'rbacapp/index.html', ctx)
+    
 # Create your views here.
--- sehack/sehack/settings.py
+++ sehack/sehack/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.getenv("django_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rbacapp'
 ]
 
 MIDDLEWARE = [
