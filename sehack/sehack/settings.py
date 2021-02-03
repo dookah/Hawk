5ee70c65df427f3dffd7a498c59f1ce26288222d
Joshua Dean <joshualukedean@gmail.com>
2021-02-03 20:56:10 +0000

    updates

--- sehack/sehack/settings.py
+++ sehack/sehack/settings.py
@@ -145,3 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
